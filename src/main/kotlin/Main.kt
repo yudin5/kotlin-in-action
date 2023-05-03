6b3674929b47c09364f4cbc47143dfb2070f412f
Vitaliy A. Yudin <vyudin@t1-consulting.ru>
2023-05-03 15:24:02 +0300

    test some new feature

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,4 +4,13 @@ fun main(args: Array<String>) {
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+
+    val size = args.size
+    println(size)
+    var newArgs = args.plus("abc")
+    newArgs = newArgs.plus("ddd")
+    newArgs = newArgs.plus("eee")
+    val lastElement = newArgs.last()
+    println("newArgs = ${newArgs.joinToString()}")
+    println("lastElement = ${lastElement}")
 }
