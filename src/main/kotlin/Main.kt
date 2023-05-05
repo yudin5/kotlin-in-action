c12f7967c485170dddd3c4a43e2f75da221e6e8a
Vitaliy A. Yudin <vyudin@t1-consulting.ru>
2023-05-05 21:59:15 +0300

    test some new feature

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,6 @@
+fun max(a: Int, b: Int) = if (a > b) a else b
+
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
@@ -5,12 +8,36 @@ fun main(args: Array<String>) {
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 
+    val name = if (args.size > 0) args[0] else "Kotlin"
+    println("Hello, $name!")
+//    println("Hello, ${args[0]}!")
+//    println("Hello, ${if (args.isNotEmpty()) args[0] else "someone"}!")
+
     val size = args.size
     println(size)
     var newArgs = args.plus("abc")
     newArgs = newArgs.plus("ddd")
     newArgs = newArgs.plus("eee")
     val lastElement = newArgs.last()
+    val firstElement = newArgs.first()
     println("newArgs = ${newArgs.joinToString()}")
-    println("lastElement = ${lastElement}")
+    println("firstElement = $firstElement")
+    println("lastElement = $lastElement")
+
+    // Вызов функции
+    println(max(4, 34))
+
+    // Объявление переменных
+    val question = "The Ultimate Question of Life, the Universe, and Everything"
+    val answer: Int = 42
+    println("Question = $question. Answer = $answer")
+    val yearsToCompute = 7.5e6
+    println("yearsToCompute = $yearsToCompute")
+
+    // Неизменные ссылки, изменяемые объекты
+    val languages = arrayListOf("Java")
+    val someList = arrayListOf("sdf sdf sdf".split(" "))
+    println("someList = $someList")
+    languages.add("Kotlin")
+    println("languages = $languages")
 }
