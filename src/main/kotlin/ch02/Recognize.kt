997d9f497857602e79fc6024be988dadbb4db5a5
yudin <yudin5@yandex.ru>
2023-05-13 16:27:27 +0300

    add recognize and readnumber

--- src/main/kotlin/ch02/Recognize.kt
+++ src/main/kotlin/ch02/Recognize.kt
@@ -1,5 +1,8 @@
 package ch02
 
+import kotlin.random.Random
+import kotlin.random.nextInt
+
 fun isLetter(c: Char) = c in 'a'..'z' || c in 'A'..'Z'
 fun isNotDigit(c: Char) = c !in '0'..'9'
 fun recognize(c: Char) =
@@ -17,4 +20,19 @@ fun main() {
     println(recognize('g'))
     println(recognize('5'))
     println(recognize('#'))
+
+    println("Kotlin" in "Java".."Scala")
+    println("Kotlin" in setOf("Java", "Scala"))
+
+    val percentage = Random.nextInt(-10..150)
+    if (percentage !in 0..100) {
+        throw IllegalArgumentException("A percentage value must be between 0 and 100: $percentage")
+    }
+
+    val percentage2 =
+        if (percentage in 20..90)
+            percentage
+        else
+            throw IllegalArgumentException("Must be between 20 and 90: $percentage")
+
 }
