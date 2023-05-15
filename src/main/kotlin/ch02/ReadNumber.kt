24cba376d3bcc4a59b839f332ce2f535bd9bc2bd
Vitaliy A. Yudin <vyudin@t1-consulting.ru>
2023-05-15 20:30:52 +0300

    refactoring ReadNumber.kt

--- src/main/kotlin/ch02/ReadNumber.kt
+++ src/main/kotlin/ch02/ReadNumber.kt
@@ -3,18 +3,17 @@ package ch02
 import java.io.BufferedReader
 import java.io.StringReader
 
-fun readNumber(reader: BufferedReader): Int? {
-    try {
+fun readNumber(reader: BufferedReader) {
+    val number = try {
         val line = reader.readLine()
-        return Integer.parseInt(line)
+        Integer.parseInt(line)
     } catch (e: NumberFormatException) {
-        return null
-    } finally {
-        reader.close()
+        null
     }
+    println(number)
 }
 
 fun main() {
-    val reader = BufferedReader(StringReader("df"))
-    println(readNumber(reader))
+    val reader = BufferedReader(StringReader("2sd"))
+    readNumber(reader)
 }
